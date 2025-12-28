e7150d0f1ca56afa822fe1332067fa7f55eeb105
Eclipse <eclipse@eclipseisoffline.xyz>
2025-12-28 10:35:21 +0000

    Bump to 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "xyz.eclipseisoffline"
-version = "0.2.0-SNAPSHOT"
+version = "0.2.0"
 
 val targetJavaVersion = 25
 
@@ -42,8 +42,6 @@ gradlePlugin {
         }
     }
 }
-
-print(System.getenv("MULTIMOD_IS_DEV"))
 
 publishing {
     repositories {
